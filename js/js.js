689323a7f657b4cb48412e125d067ae925019e3e
Ertugrul <iam@ertugrulkat.nl>
2013-06-27 23:54:45 +0200

    Fu Bahtiyar

--- js/js.js
+++ js/js.js
@@ -1,56 +1,46 @@
 $("#slider").fadeIn();
 
+var i = 2;
+var imageClassFix = 0;
 var slider = {
 
-	sliding: $("#slider"),
-	image: 'img/screen*.png',
-	auto: true,
-	delay: 5000,
-	last: 0,
-
+    sliding: $("#slider"),
+    image: 'img/screen*.png',
+    auto: true,
+    delay: 5000,
+    last: i,
     slide: function() {
-		var i = 2;
-		if(slider.auto = true) {
-			setInterval(function() {
-				for(var a = 1; a < 6; a++) {
-					$("#" + a).removeClass('shadow');
-				}
-				if(slider.last = 0) {
-					$('#' + slider.last).addClass('shadow');
-				}
-				else {
-					$('#' + i).addClass('shadow');
-				}
-			    var path = slider.image.replace('*', i);
-			    slider.sliding.fadeOut(function() {
-				    $(this).attr("src", path).fadeIn();
-				});
-			    i = i + 1;
-			    if (i == 6) i = 1;
-			}, slider.delay);
-		}
-    },
-
-    stopslide: function() {
-    	slider.auto = false;
-    	slider.last = 0;
-    	for(var i = 1; i < 6; i++) {
-    		$("#" + i).removeClass('shadow');
-    	}
+	   $("#expo-container").children("article").mouseenter(function(){
+		  slider.auto = false;
+		  console.log(slider.auto);
+	   });
+	   if(slider.auto == false)
+	   {
+		 
+	   }
+	   else
+	   {
+		 var play = setInterval(function() {
+			 $("#expo-container").children("article").each(function()
+			 {
+				$(this).removeClass("shadow");
+			 })
+			 var path = slider.image.replace('*', i);
+			 slider.sliding.fadeOut(function() {
+				$(this).attr("src", path).fadeIn();
+				$("#"+imageClassFix).addClass("shadow");
+			 });
+		  
+			 i = i + 1;
+			 imageClassFix = i - 1;
+			 if (i == 6) i = 1;
+			 slider.last = i;
+	
+		  }, slider.delay);
+	
+	   }
     }
 }
 
-$('.expo-block').hover(function(){
-	var id = $(this).attr('id');
-	$('#slider').fadeOut();
-	$("#slider").attr('src', 'img/screen' + id +'.png').fadeIn();
-	slider.stopslide();
-});
-
-$('.expo-block').mouseout(function(){
-	slider.auto = true;
-	slider.last = $(this).attr("id");
-	slider.slide();
-});
-
 slider.slide();
+
